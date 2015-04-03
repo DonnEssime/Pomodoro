3b56fe1f61dc125e0fdc34cf19b4643d6049dfe7
Simon Donne <donnesimon@gmail.com>
2015-04-03 21:19:30 +0200

    PomodoroUI is now the Frame
    Which makes much more sense from a logical point of view.
    But, is the PomodoroMainFrame still a required layer of abstraction?

--- src/pomodoro/PomodoroMainFrame.java
+++ src/pomodoro/PomodoroMainFrame.java
@@ -1,33 +1,12 @@
 package pomodoro;
 
-import java.awt.*;
-import java.awt.event.*;
 
-public class PomodoroMainFrame extends Frame implements Runnable {
-    private static javax.swing.ImageIcon icon = Util.getIconImage("/ui/icon.png");
+public class PomodoroMainFrame implements Runnable {
     private PomodoroManager mgr;
-    private Thread t;
     
     public PomodoroMainFrame()
     {
-        super("Pomodoro Timer");
         mgr = new PomodoroManager();
-        
-        this.setSize(640,320);
-        this.setIconImage(icon.getImage());
-        this.setResizable(false);
-	addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                t.interrupt();
-                try {
-                    t.join();
-                } catch (InterruptedException ex) {
-                }
-                System.exit(0);
-            }
-        });
-        this.setVisible(true);
     }
     
     static {
@@ -48,7 +27,6 @@ public class PomodoroMainFrame extends Frame implements Runnable {
         System.out.println("Mainframe starting up");
         ThreadGroup tg = new ThreadGroup("Pomodoro main group");
         Thread t = new Thread(tg, mgr);
-        this.t = Thread.currentThread();
         t.start();
         try {
             t.join();
--- src/pomodoro/PomodoroUI.java
+++ src/pomodoro/PomodoroUI.java
@@ -1,18 +1,47 @@
 package pomodoro;
 
-public class PomodoroUI implements Runnable {
+import java.awt.Frame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.List;
+import java.util.LinkedList;
+
+public class PomodoroUI extends Frame implements Runnable {
+    private static javax.swing.ImageIcon icon = Util.getIconImage("/ui/icon.png");
     private final PomodoroManager mgr;
     private boolean running = false;
+    private final List<Widget> children;
+    private Thread t;
     
     public PomodoroUI(PomodoroManager mgr)
     {
+        super("Pomodoro Timer");
+        
         this.mgr = mgr;
+        this.children = new LinkedList<Widget>();
+        
+        this.setSize(640,320);
+        this.setIconImage(icon.getImage());
+        this.setResizable(false);
+	addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                t.interrupt();
+                try {
+                    t.join();
+                } catch (InterruptedException ex) {
+                }
+                System.exit(0);
+            }
+        });
+        this.setVisible(true);
     }
         
     @Override
     public void run() {
         running = true;
         System.out.println("UI starting up");
+        this.t = Thread.currentThread();
         while(!Thread.currentThread().isInterrupted())
         {
             
